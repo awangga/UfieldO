13043f7a53aca2a360cd89ffae7dabf12a11caf0
Rolly Maulana Awangga <rolly@awang.ga>
2016-09-19 22:20:16 +0700

    add configuration

---
+++ config-sample.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+"""
+updater.py library
+"""
+
+ORACLE_HOME = "/usr/lib/oracle/12.1/client64"
+LD_LIBRARY_PATH = "/usr/lib/oracle/12.1/client64/lib/"
+
+username = "username"
+password = "password"
+host = "localhost"
+port = "1521"
+sid = "orcl"
+
